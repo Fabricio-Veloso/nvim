5a26d912014ac1b84e0b2924b85a483925fa6640
Fabricio-Veloso <fabraccho@gmail.com>
2025-05-14 11:42:44 -0300

    Adding neotree travel to active buffer keymap. Very usefull.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -67,3 +67,6 @@ vim.keymap.set('o', 'j', 'h', { noremap = true, silent = true })
 vim.keymap.set('o', 'k', 'j', { noremap = true, silent = true })
 vim.keymap.set('o', 'l', 'k', { noremap = true, silent = true })
 vim.keymap.set('o', 'ç', 'l', { noremap = true, silent = true })
+
+-- to travel to active buffer directory
+vim.keymap.set('n', '<leader>f', ':Neotree reveal<CR>')
