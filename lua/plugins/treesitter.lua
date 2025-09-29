13e47321e68d8254d54a132d8fa404019bfc0dc1
FabricioVeloso <fabraccho@gmail.com>
2025-09-28 22:19:02 -0300

    fixig treesetter config.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,13 @@
-return { -- Highlight, edit, and navigate code
+return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = { 'razor', 'bash', 'c_sharp', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
+    ensure_installed = { 'bash', 'html', 'markdown', 'markdown_inline', 'vim', 'vimdoc' }, -- sem lua nem c_sharp
     auto_install = true,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby', 'razor', 'html' },
+      additional_vim_regex_highlighting = { 'html' },
     },
-    indent = { enable = true, disable = { 'ruby', 'html', 'c_sharp' } },
+    indent = { enable = true, disable = { 'html' } },
   },
 }
