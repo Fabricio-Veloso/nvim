f556a4683023351c9b4f7930996913d0162d8fe6
FabricioVeloso <fabraccho@gmail.com>
2025-10-23 15:05:07 -0300

    Add config for the lf plugin.

---
+++ lua/plugins/lf.lua
@@ -0,0 +1,27 @@
+return {
+  "ptzz/lf.vim",
+  dependencies = { "voldikss/vim-floaterm" },
+  config = function()
+    local map = vim.keymap.set
+
+    -- Corrige o mapeamento: remove qualquer <leader>f criado pelo LF
+    vim.schedule(function()
+      pcall(vim.keymap.del, 'n', '<leader>f')  -- apaga o que o LF criou
+      map('n', '<leader>f', ':Neotree reveal<CR>', { desc = "Abrir Neo-tree no arquivo atual" })
+    end)
+
+    -- Mapeamento separado para abrir o LF manualmente
+    map('n', '<leader>lf', ':Lf<CR>', { desc = "Abrir LF" })
+
+    -- Configurações do LF
+    vim.g.lf_netrw = 0
+    vim.g.lf_preview = 1
+    vim.g.lf_height = 0.85
+    vim.g.lf_width = 0.85
+    vim.g.lf_replace_netrw = 0
+    vim.g.lf_split = "v"
+    vim.g.lf_command = "lf"
+    vim.g.lf_fzf = 1
+    vim.g.lf_rg = 1
+  end,
+}
