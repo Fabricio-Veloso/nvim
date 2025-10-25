44a297b435409ef08a1d55cf7f2ce5d5791fa6eb
FabricioVeloso <fabraccho@gmail.com>
2025-10-25 11:48:27 -0300

    comment python dap (is broken).

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -2,7 +2,7 @@ return {
   require("plugins.dap.nvim-dap"),
   require("plugins.dap.dap-ui"),
   require("plugins.dap.dap-csharp"),
-  require("plugins.dap.dap-python"),
+--  require("plugins.dap.dap-python"),
   require("plugins.dap.dap-js"),
   require("plugins.dap.dap-keymaps"),
 }
