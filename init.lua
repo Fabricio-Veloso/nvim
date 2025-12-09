fa30144913501abdd01ce3cfb8c948a8ee2d234a
FabricioVeloso <fabraccho@gmail.com>
2025-12-09 08:37:35 -0300

    Add autossesion to test.

--- init.lua
+++ init.lua
@@ -88,6 +88,7 @@ require('lazy').setup {
   require('plugins.mcphub-nvim'),
   require('plugins.neogit'),
   require('plugins.markdown-preview'),
+  require('plugins.auto-session')
 }
 
 require('make')
